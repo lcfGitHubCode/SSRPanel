bf267e9f08dc8b770910105207e03da8b29a59c1
lcfGitHubCode <15369332075@163.com>
2018-05-15 19:55:56 -0400

    fix

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => '3306',
             'database' => 'ssrpanel',
             'username' => 'root',
-            'password' => 'Mysqlpassw0rd.',
+            'password' => 'root',
             'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
